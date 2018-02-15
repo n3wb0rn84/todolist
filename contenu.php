4556e76ce92b1d559e18db58e318599b6042a18d
n3wb0rn84 <o.courbe@gmail.com>
2018-02-15 16:33:12 +0100

    Eten moins de temps que Dieu ne créa l'homme

---
+++ contenu.php
@@ -0,0 +1,37 @@
+<?php
+  $task1 = file_get_contents('assets/JSON/todo.json',true);
+  $taskArray1 = json_decode($task1);
+  $todo1 = $taskArray1->todo;
+  $done1 = $taskArray1->done;
+  if (isset($_POST['checkbox']) && $_POST['checkbox'] != ''){
+  $checked = $_POST['checkbox'];
+  var_dump(array_search($checked, $todo1));
+  array_push($taskArray1->done, $todo1[$checked]);
+  unset($taskArray1->todo[$checked]);
+  $Wallah=json_encode($taskArray1);
+  file_put_contents('assets/JSON/todo.json',$Wallah);
+  header('location: index.php');
+}
+
+
+ ?>
+<h2>A FAIRE</h2>
+<form class="" action="index.php" method="post">
+<ul>
+  <?php
+  foreach ($todo1 as $key=>$value){
+  ?>
+  <li><input type="checkbox" name="checkbox" value="<?=$key?>"><?=$value?></li>
+<?php }?>
+<button type="submit" name="button">Enregistrer</button>
+</ul>
+</form>
+<h2>ARCHIVE</h2>
+<ul>
+  <?php
+  foreach ($done1 as $key=>$value){
+  ?>
+  <li class="done"><input type="checkbox" name="checkbox" value="<?=$key?>" checked="true"><?=$value?></li>
+<?php }?>
+<?php ?>
+</ul>
---
+++ formulaire.php
@@ -0,0 +1,19 @@
+<?php
+  $task = file_get_contents('assets/JSON/todo.json',true);
+  $taskArray = json_decode($task);
+  $taskToAdd = $_POST['textArea'] ?? '';
+  if ($taskToAdd != '' && isset($taskToAdd)){
+    array_push($taskArray->todo, $taskToAdd);
+    $Wallah=json_encode($taskArray);
+    file_put_contents('assets/JSON/todo.json',$Wallah);
+    header('location: index.php');
+  }
+
+ ?>
+
+ <form class="" action="index.php" method="post">
+ <h2>Ajouter une tâche</h2>
+ <caption>La tâche à effectuer</caption>
+ <textarea name="textArea" rows="4" cols="40" placeholder="Add Task Here"></textarea>
+ <button type="submit" name="button">Ajouter</button>
+ </form>
