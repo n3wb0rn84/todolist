3f01cbafc149c6de62ba7403f80aefd6babd53fa
n3wb0rn84 <o.courbe@gmail.com>
2018-02-19 16:40:58 +0100

    heroku clap 4eme

--- formulaire.php
+++ formulaire.php
@@ -2,7 +2,7 @@
   $task = file_get_contents('assets/JSON/todo.json',true);
   $taskArray = json_decode($task);
   $taskToAdd = $_POST['textArea'] ?? '';
-  if ($taskToAdd != '' && isset($taskToAdd)){
+  if ($taskToAdd != ''){
     array_push($taskArray->todo, $taskToAdd);
     $Wallah=json_encode($taskArray);
     file_put_contents('assets/JSON/todo.json',$Wallah);
