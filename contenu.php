8a657466b037a54c52e27b9ed3858953b0c0e1be
n3wb0rn84 <o.courbe@gmail.com>
2018-02-19 16:51:50 +0100

    heroku clap 4eme

--- contenu.php
+++ contenu.php
@@ -1,11 +1,17 @@
 <?php
   $task1 = file_get_contents('assets/JSON/todo.json',true);
   $taskArray1 = json_decode($task1);
+  $taskToAdd = $_POST['textArea'] ?? '';
+  if ($taskToAdd != ''){
+    array_push($taskArray1->todo, $taskToAdd);
+    $Wallah=json_encode($taskArray1);
+    file_put_contents('assets/JSON/todo.json',$Wallah);
+    header('location:index.php');
+  }
   $todo1 = $taskArray1->todo;
   $done1 = $taskArray1->done;
     if (isset($_POST['checkbox'])){
       foreach ($_POST['checkbox'] as $value) {
-
         array_push($taskArray1->done, $taskArray1->todo[$value]);
         unset($taskArray1->todo[$value]);
       }
@@ -43,5 +49,4 @@
     <textarea name="textArea" rows="4" cols="40" placeholder="Add Task Here"></textarea>
     <button type="submit" name="button">Ajouter</button>
   </form>
-  <?php include "formulaire.php";  ?>
 </div>
